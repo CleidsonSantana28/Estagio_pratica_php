5d139917c8d1dd51bb95fdbd1bb9ab54864236a9
cleidson santana <cleidsonsantana@gmail.com>
2022-11-24 08:47:18 -0300

    adicionando anotaçõe sobre o array associativo

--- App/arrayForeach.php
+++ App/arrayForeach.php
@@ -1,5 +1,7 @@
 <?php
-
+# Array associativo
+# e definido a variável $key como a chave, e o variável $value refere-se a ao valor da chave
+# Nesse foreach irá execultar os ciclos ate que todos os dados dentro do array se esgotem
 $nomes=['cleidson','eduarda','lucas','marilene'];
 foreach($nomes as $nome){
     echo $nome.'<br>';
